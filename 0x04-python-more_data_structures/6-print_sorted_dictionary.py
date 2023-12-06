0c26bc0ed5a94dbfbfd46aa70158f2dcfe85ee99
Zyncol <isaacyilipochipofya@gmail.com>
2023-12-06 15:16:09 -0800

    changed

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # prints dictionary by keys
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(ze, a_dictionary[ze])) for ze in sorted(a_dictionary)]
+    [print("{}: {}".format(zel, a_dictionary[zel])) for zel in sorted(a_dictionary)]
