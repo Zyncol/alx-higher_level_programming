09360d7a3e65e6e4e5e74b619f534cef621c6135
Zyncol <isaacyilipochipofya@gmail.com>
2024-01-06 14:12:30 -0800

    fixed

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -41,6 +41,7 @@ class Rectangle:
     def __del__(self):
         """Deleting a rectangle"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def height(self):
