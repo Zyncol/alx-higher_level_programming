3681bde68a5da8b98e0cdce155b2ed6c3b53d4df
Zyncol <isaacyilipochipofya@gmail.com>
2024-01-06 13:00:08 -0800

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,61 @@
-ghjkl
+#!/usr/bin/python3
+"""task 2-rectangle
+a Rectangle class modified
+"""
+class Rectangle:
+    """Rectangle class width and height defined"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing a rectangle instance
+            width: width of the rectangle side
+            height: height of the rectangle side
+        """
+        self.width = width
+        self.height = height
+
+        @property
+    def height(self):
+        """a rectangle instance height is set"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setting the height
+            value: it must be a positive integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+        @property
+    def width(self):
+        """the width of a Rectangle instance."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setting the width
+            value:it must be a positive integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """Calculates the area of a rectangle
+        Return:
+            Area of the the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculates the perimeter of rectangle
+        Return: Perimeter of the rectangle
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
