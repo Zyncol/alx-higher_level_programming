7b51900c831bcf5cac3ef66d1316d26e352dc0d2
Zyncol <isaacyilipochipofya@gmail.com>
2024-01-15 15:08:15 -0800

    trying

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""class Rectangle inherits fro the Base"""
+"""class Rectangle inherits from Base"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class inheriting from Base class"""
+    """class inheriting from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """class constructor with attri"""
+        """class constructor"""
 
         super().__init__(id)
         self.width = width
